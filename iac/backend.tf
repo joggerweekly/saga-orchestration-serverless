fa5933169b3ee7d1315b2b484e68e7cfeb36a02f
joggerweekly <78917625+joggerweekly@users.noreply.github.com>
2022-11-06 22:54:17 -0800

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -3,6 +3,7 @@ terraform {
     # modify the values for the storage account
     storage_account_name = "stsbctest01"
     container_name       = "sample-container"
+    key                  = "terraform.tfstate"
     access_key           = "6vtu7v5zOmo21rzpP8rgyi0f9fBxkM/KUDCAPxS8Livvbjv3jEFK21iMDivFIE5vukw6yl3S1QtD+AStZAIm+w=="
   }
 }
