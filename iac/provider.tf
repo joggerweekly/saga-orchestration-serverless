7d8a15720b3a23c450bbf77753bffbc1b50497ee
joggerweekly <78917625+joggerweekly@users.noreply.github.com>
2022-11-07 13:10:23 -0800

    Update provider.tf

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version         = "= 2.1.0"
+  version         = "= 3.30.0"
   subscription_id = var.sub
   client_id       = var.client_id
   client_secret   = var.client_secret
