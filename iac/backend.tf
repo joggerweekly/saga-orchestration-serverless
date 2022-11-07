f3bea6e76551ecd2c60e64b4db2dd2357ee10f5c
joggerweekly <78917625+joggerweekly@users.noreply.github.com>
2022-11-06 21:54:08 -0800

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     # modify the values for the storage account
-    storage_account_name = "sagaorchestratorstorage"
-    container_name       = "testingterraform"
-    key                  = "terraform.tfstate"
+    storage_account_name = "stsbctest01"
+    container_name       = "sample-container"
+    key                  = "6vtu7v5zOmo21rzpP8rgyi0f9fBxkM/KUDCAPxS8Livvbjv3jEFK21iMDivFIE5vukw6yl3S1QtD+AStZAIm+w=="
   }
 }
