724aa24f502b16d2ae680022e9de888221e1a7c7
joggerweekly <78917625+joggerweekly@users.noreply.github.com>
2022-11-06 22:52:01 -0800

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "azurerm" {
     # modify the values for the storage account
     storage_account_name = "stsbctest01"
-    container_name       = "terraform.tfstate"
+    container_name       = "testingterraform"
+    key                  = "terraform.tfstate"
     access_key           = "6vtu7v5zOmo21rzpP8rgyi0f9fBxkM/KUDCAPxS8Livvbjv3jEFK21iMDivFIE5vukw6yl3S1QtD+AStZAIm+w=="
   }
 }
